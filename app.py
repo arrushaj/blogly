4ec9679f8ad1a5bf1fa871b40c4b581c17e63015
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-12-07 14:30:13 -0600

    blah

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,38 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def list_users():
+    """List pets and show add form."""
+
+    users = User.query.all()
+    return render_template("list.html", users=users)
+
+
+@app.post("/")
+def add_user():
+    """Add pet and redirect to list."""
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(f"/{user.id}")
+
+
+@app.get("/<user_id>")
+def show_user(user_id):
+    """Show info on a single user."""
+
+    user = User.query.get_or_404(int(user_id))
+    return render_template("detail.html", user=user)
